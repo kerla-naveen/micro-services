df860dde014757bd690233a4638dbf2f52169768
Naveen Kerla <kerlanaveen19@gmail.com>
2026-01-26 15:06:42 +0530

    impelemented feign-client for calling productService

--- src/main/java/com/DigiClassRoom/OrderService/service/OrderServiceImpl.java
+++ src/main/java/com/DigiClassRoom/OrderService/service/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.DigiClassRoom.OrderService.service;
 
 import com.DigiClassRoom.OrderService.entity.Order;
+import com.DigiClassRoom.OrderService.external.client.ProductService;
 import com.DigiClassRoom.OrderService.model.OrderRequest;
 import com.DigiClassRoom.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
@@ -16,10 +17,16 @@ public class OrderServiceImpl implements OrderService{
     @Autowired
     OrderRepository orderRepository;
 
+    @Autowired
+    ProductService productService;
+
     @Override
     public Long placeOrder(OrderRequest orderRequest) {
         // store/place the oder
         log.info("placing the order :{}",orderRequest);
+        productService.reduceQuantity(orderRequest.getProductId(), orderRequest.getQuantity());
+        log.info("order CREATED :{}",orderRequest);
+
         Order order= Order.builder()
                 .productId(orderRequest.getProductId())
                 .amount(orderRequest.getTotalAmount())

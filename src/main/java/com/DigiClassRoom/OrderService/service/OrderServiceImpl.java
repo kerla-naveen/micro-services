1ef06f72b96e0238818597d75a6ad4fe3eca7242
Naveen Kerla <kerlanaveen19@gmail.com>
2026-01-30 00:02:19 +0530

    implemented fegin-client for payment-service and called doPayment in placeOrder APi

--- src/main/java/com/DigiClassRoom/OrderService/service/OrderServiceImpl.java
+++ src/main/java/com/DigiClassRoom/OrderService/service/OrderServiceImpl.java
@@ -1,7 +1,9 @@
 package com.DigiClassRoom.OrderService.service;
 
 import com.DigiClassRoom.OrderService.entity.Order;
+import com.DigiClassRoom.OrderService.external.client.PaymentService;
 import com.DigiClassRoom.OrderService.external.client.ProductService;
+import com.DigiClassRoom.OrderService.external.request.PaymentRequest;
 import com.DigiClassRoom.OrderService.model.OrderRequest;
 import com.DigiClassRoom.OrderService.repository.OrderRepository;
 import lombok.extern.log4j.Log4j2;
@@ -20,10 +22,14 @@ public class OrderServiceImpl implements OrderService{
     @Autowired
     ProductService productService;
 
+    @Autowired
+    PaymentService paymentService;
+
     @Override
     public Long placeOrder(OrderRequest orderRequest) {
         // store/place the oder
         log.info("placing the order :{}",orderRequest);
+        // lock/reduce quantity in productService
         productService.reduceQuantity(orderRequest.getProductId(), orderRequest.getQuantity());
         log.info("order CREATED :{}",orderRequest);
 
@@ -34,9 +40,28 @@ public class OrderServiceImpl implements OrderService{
                 .orderDate(Instant.now())
                 .build();
         orderRepository.save(order);
-        log.info("order is placed successfully with orderId:{}",order.getId());
-        // lock/reduce qunatity in productService
+        log.info("order is saved successfully with orderId:{}",order.getId());
+
+        log.info("calling payment service");
+        PaymentRequest paymentRequest= PaymentRequest.builder()
+                .oderId(order.getId())
+                .paymentMode(orderRequest.getPaymentMode())
+                .amount(orderRequest.getQuantity())
+                .build();
+
+        String orderStatus=null;
+        try {
+            paymentService.doPayment(paymentRequest);
+            orderStatus="SUCCESS";
+            log.info("payment is successful for order id:{}",order.getId());
+        } catch (Exception e) {
+            orderStatus="FAILED";
+            log.info("error in payment for order id:{}",order.getId());
+        }
+        order.setOrderStatus(orderStatus);
+        orderRepository.save(order);
         // payment service -success/ failure / rejected
+
         return order.getId();
     }
 }

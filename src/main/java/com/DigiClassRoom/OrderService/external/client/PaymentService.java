ffe820111ef100690dd7f8553494f87e623e41b8
Naveen Kerla <kerlanaveen19@gmail.com>
2026-02-03 02:27:09 +0530

    implement circuitBreaker with fallback methods

--- src/main/java/com/DigiClassRoom/OrderService/external/client/PaymentService.java
+++ src/main/java/com/DigiClassRoom/OrderService/external/client/PaymentService.java
@@ -1,14 +1,25 @@
 package com.DigiClassRoom.OrderService.external.client;
 
+import com.DigiClassRoom.OrderService.exception.CustomException;
 import com.DigiClassRoom.OrderService.external.request.PaymentRequest;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@CircuitBreaker(name= "external", fallbackMethod = "fallback")
 @FeignClient(name = "PAYMENT-SERVICE", path = "payment/")
 public interface PaymentService {
 
     @PostMapping
     ResponseEntity<Long> doPayment(@RequestBody PaymentRequest paymentRequest);
+
+    default  void fallback(Exception e){
+        throw new CustomException(
+                "payment service not available",
+                "UNAVAILABLE",
+                500
+                );
+    }
 }
--- src/main/java/com/DigiClassRoom/OrderService/external/client/ProductService.java
+++ src/main/java/com/DigiClassRoom/OrderService/external/client/ProductService.java
@@ -1,11 +1,14 @@
 package com.DigiClassRoom.OrderService.external.client;
 
+import com.DigiClassRoom.OrderService.exception.CustomException;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CircuitBreaker(name = "external", fallbackMethod = "fallback")
 @FeignClient(name= "PRODUCT-SERVICE" , path="/product")
 public interface ProductService {
 
@@ -13,4 +16,12 @@ public interface ProductService {
     ResponseEntity<Void> reduceQuantity(
             @PathVariable("id") long productId, @RequestParam long quantity
     );
+
+    default void fallback(Exception e){
+        throw new CustomException(
+                "product service not available",
+                "UNAVAILABLE",
+                500
+        );
+    }
 }

37334514c305d9463fb9a2cf7d672436a5372fc4
Naveen Kerla <kerlanaveen19@gmail.com>
2026-03-01 01:46:09 +0530

    Refactor to support runtime refresh of remote configuration

--- src/main/java/com/DigiClassRoom/paymentService/model/PaymentAPIContactInfo.java
+++ src/main/java/com/DigiClassRoom/paymentService/model/PaymentAPIContactInfo.java
@@ -1,10 +1,19 @@
 package com.DigiClassRoom.paymentService.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "payment")
-public record PaymentAPIContactInfo(String message, Map<String,String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class PaymentAPIContactInfo {
+    private String message;
+    private Map<String,String> contactDetails;
+    private List<String> onCallSupport;
 }
